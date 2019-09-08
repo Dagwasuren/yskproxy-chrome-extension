28da8dab5a5509de0e7c0873a3610486377121d4
ZhiFeng Hu <hufeng1987@gmail.com>
2019-09-08 22:34:55 +0800

    add set fixed proxy server

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-    let pacUrl = 'http://localhost:8085/pac';
-    chrome.storage.sync.set({yskproxyPacUrl: pacUrl}, function() {
-      console.log("yskproxyPacUrl:" + pacUrl);
-    });
-  });
--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 console.log("yskproxy loaded")
-let pInput = document.getElementById("pacUrl")
 
 function getElm(s) {
   return document.getElementById(s);
 }
+
+let pInput = getElm("pacUrl")
+let mpsInput = getElm("manualProxyScheme")
+let mphInput = getElm("manualProxyHost")
+let mppInput = getElm("manualProxyPort")
 
 function jsonPretty(s) {
   return JSON.stringify(s, undefined, 2);
@@ -16,9 +20,29 @@ function displayCurrentProxyConfig() {
   })
 }
 
-chrome.storage.sync.get('yskproxyPacUrl', function (data) {
-  pInput.value = data.yskproxyPacUrl
-  console.log(data)
+chrome.storage.sync.get([
+  'yskproxyPacUrl',
+  'yskproxyManualProxyScheme',
+  'yskproxyManualProxyHost',
+  'yskproxyManualProxyPort',
+  ], function (data) {
+    console.log(data);
+    let dkeys = Object.keys(data);
+    if (dkeys.indexOf("yskproxyPacUrl") > -1) {
+      pInput.value = data.yskproxyPacUrl;
+    }
+    if (dkeys.indexOf("yskproxyManualProxyScheme") > -1) {
+      mpsInput.value = data.yskproxyManualProxyScheme;
+    } 
+    if (dkeys.indexOf("yskproxyManualProxyHost") > -1) {
+      mphInput.value = data.yskproxyManualProxyHost;
+    }
+    
+    if (dkeys.indexOf("yskproxyManualProxyPort") > -1) {
+      mppInput.value = data.yskproxyManualProxyPort;
+    }
+    
+  
 })
 
 displayCurrentProxyConfig();
@@ -28,7 +52,7 @@ cbtn.onclick = function () {
   var date = new Date();
   var ts = date.getTime();
   var pacUrl = document.getElementById("pacUrl").value;
-  pacUrl = pacUrl + "?t=" + ts;
+  npacUrl = pacUrl + "?t=" + ts;
   console.log(pacUrl);
   chrome.storage.sync.set({ yskproxyPacUrl: pacUrl }, function () {
     console.log('pacUrl: ' + pacUrl)
@@ -36,12 +60,46 @@ cbtn.onclick = function () {
   var config = {
     mode: "pac_script",
     pacScript: {
-      url: pacUrl
+      url: npacUrl
     }
   };
   chrome.proxy.settings.set(
     { value: config, scope: 'regular' },
     function () { });
+
+  displayCurrentProxyConfig();
+
+}
+
+let mbtn = document.getElementById("changeManualProxy")
+mbtn.onclick = function () {
+  var mps = getElm("manualProxyScheme").value; 
+  var mph = getElm("manualProxyHost").value; 
+  var mpp = getElm("manualProxyPort").value; 
+  chrome.storage.sync.set({
+     yskproxyManualProxyScheme: mps,
+     yskproxyManualProxyHost: mph,
+     yskproxyManualProxyPort: mpp
+     }, function () {
+      console.log('manualProxyScheme: ' + mps);
+      console.log('manualProxyHost: ' + mph);
+      console.log('manualProxyPort: ' + mpp);
+  })
+ 
+  var config = {
+    mode: "fixed_servers",
+    rules: {
+      singleProxy: {
+        scheme: mps,
+        host: mph,
+        port: Number(mpp)
+      },
+      bypassList: []
+    }
+  };
+  chrome.proxy.settings.set(
+      {value: config, scope: 'regular'},
+      function() {});
 
   displayCurrentProxyConfig();
 
